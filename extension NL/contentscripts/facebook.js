a9db258323fa9c384e3e315775ecbbc6edb21283
Michelle Johnson <mchlljy@mit.edu>
2013-12-10 11:15:28 -0500

    removed unnecessary file

--- extension NL/contentscripts/facebook.js
+++ extension NL/contentscripts/facebook.js
@@ -232,7 +232,18 @@ chrome.extension.sendRequest({type: "getUserSegment"},
 			}
 		}
 
-		
+		console.log('extension click there?', $('.extension-click'));
+		$('.extension-click').on('click', function(event) {
+			console.log('clicked');
+
+			  $(window).bind('storage', function (e) {
+			    console.log('storage changed');
+			  });
+
+			  localStorage.setItem('a', 'test');
+
+			chrome.extension.sendRequest({type: "writeClick", stimID: "3", target: event.target.getAttribute("clicktext")});
+		});
 
 		//Ensures that script runs when they click on FB logo or Home buttton
 		$("#pageLogo").click(function() {
